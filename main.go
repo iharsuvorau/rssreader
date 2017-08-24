d1769e41f8268b8782d664c366a2287ac4c9fed7
Ihar Suvorau <ihar.suvorau@gmail.com>
2017-08-24 23:14:41 +0300

    profiling setup

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
 func main() {
 	url := flag.String("a", "", "add a feed's URL to fetch")
 	list := flag.Bool("l", false, "show a list of current feeds")
 	show := flag.String("s", "", "show a list of items for a feed with the specified index")
+
+	cpuprof := flag.String("cpu", "", "write cpu profile to the file")
+	memprof := flag.String("mem", "", "write memory profile to the file")
+
 	flag.Parse()
 
 	fdb := FileDatabase{
@@ -19,6 +26,33 @@ func main() {
 	}
 
 	var err error
+
+	// pprof
+
+	if *cpuprof != "" {
+		f, err := os.Create(*cpuprof)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		if err = pprof.StartCPUProfile(f); err != nil {
+			log.Fatal(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if *memprof != "" {
+		f, err := os.Create(*memprof)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		if err = pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// app logic
 
 	if err = fdb.init(); err != nil {
 		fmt.Println(err)
